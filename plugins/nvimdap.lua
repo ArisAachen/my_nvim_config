e1ce546fcf477fcd75313913482190a188e143eb
ArisAachen <aachenehurchan@gmail.com>
2022-12-16 18:20:21 +0800

    add nvim adapter

--- plugins/nvimdap.lua
+++ plugins/nvimdap.lua
@@ -95,3 +95,30 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close({})
 end
+
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = '/home/aris/.local/share/nvim/mason/bin/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
